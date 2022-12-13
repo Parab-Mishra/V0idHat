b8da3eac4bb44756714ac0476c685ae54dcd70dc
Parab Mishra <parab.mishra@ajjas.com>
2022-12-13 22:33:30 +0530

    added voidhat.medium webvieww

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from "expo-constants";
 
 export default function App() {
   return (
     <WebView
       style={styles.container}
+      userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
       source={{ uri: 'https://voidhat.medium.com' }}
     />
   );
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Constants.statusBarHeight
   },
 });
