e37b80663d7c22657df740722f5d30d116de50ca
Parab Mishra <parab.mishra@ajjas.com>
2022-12-14 21:49:56 +0530

    added splash screen and logo

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.parentContainer}>
       <WebView
         style={styles.container}
-        userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        userAgent={"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}
         source={{ uri: 'https://voidhat.medium.com/' }}
       />
     </View>
